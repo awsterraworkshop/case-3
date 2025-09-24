328be3f5cb09e9f3b24d3a45c018816219e27606
himanshu <rathihimanshu@live.com>
2025-09-24 14:01:08 +0530

    final

--- app.tf
+++ app.tf
@@ -1,11 +1,18 @@
-# resource "aws_instance" "app" {
-#   ami           = data.aws_ami.amzonami.id
-#   instance_type = "t3.medium"
-#   subnet_id     = aws_subnet.case3-private-subnet[0].id
-#   security_groups = [aws_security_group.case3-private-app.id]
-#   user_data = file("user_data_app.sh")
+resource "aws_instance" "app" {
+  ami           = data.aws_ami.amzonami.id
+  instance_type = "t3.medium"
+  subnet_id     = aws_subnet.case3-private-subnet[0].id
+  security_groups = [aws_security_group.case3-private-app.id]
+  user_data = file("user_data_app.sh")
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp3"
+    delete_on_termination = true
+  }
+  tags = {
+    Name = "case3-application-server"
+  }
+}
 
-#   tags = {
-#     Name = "case3-application-server"
-#   }
-# }
+
+
